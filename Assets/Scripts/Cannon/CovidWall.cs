76b7e00454775405cba9437dab4a9d3fc343aa83
TomeuGarcia <tomeu.garcia@enti.cat>
2022-07-02 21:35:21 +0200

    Updated canon

--- Assets/Scripts/Cannon/CovidWall.cs
+++ Assets/Scripts/Cannon/CovidWall.cs
@@ -11,18 +11,25 @@ public class CovidWall : MonoBehaviour
 
     [SerializeField] Transform wallTransform;
 
+    private int numberOfPlayersOutOfAmmo = 0;
+
+
+    public delegate void CovidWallAction();
+    public static event CovidWallAction OnAllPlayersOutOfAmmo;
+
 
     private void OnEnable()
     {
         Weakspot.OnDeath += WeakspotWasDestoyed;
         CannonUse.OnPlayerDismount += () => destroyedWeakspotAmount = 0;
+        CannonUse.OnPlayerRunsOutOfAmmo += DoPlayerOutOfAmmo;
     }
 
     private void OnDisable()
     {
         Weakspot.OnDeath -= WeakspotWasDestoyed;
         CannonUse.OnPlayerDismount -= () => destroyedWeakspotAmount = 0;
-
+        CannonUse.OnPlayerRunsOutOfAmmo -= DoPlayerOutOfAmmo;
     }
 
 
@@ -32,8 +39,6 @@ public class CovidWall : MonoBehaviour
 
         if (destroyedWeakspotAmount >= weakspotAmount)
         {
-
-
             StartCoroutine(DestroyedAnimation());
         }
 
@@ -44,5 +49,16 @@ public class CovidWall : MonoBehaviour
         yield return new WaitForSeconds(2f);
         wallTransform.DOPunchScale(new Vector3(0.4f, 0.2f, 0f), 2f).OnComplete(() => gameObject.SetActive(false));
     }
+
+    private void DoPlayerOutOfAmmo()
+    {
+        if (++numberOfPlayersOutOfAmmo == 2)
+        {
+            if (OnAllPlayersOutOfAmmo != null) OnAllPlayersOutOfAmmo();
+            numberOfPlayersOutOfAmmo = 0;
+        }
+    } 
+
+
 
 }
