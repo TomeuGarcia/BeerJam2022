5246cc6deccab7784aea3f0411da00b53a2345c0
David <david.soriano@enti.cat>
2022-07-03 13:31:33 +0200

    Pause Menu in progress

--- Assets/Scripts/MenuManagment/MusicPlay.cs
+++ Assets/Scripts/MenuManagment/MusicPlay.cs
@@ -1,14 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class MusicPlay : MonoBehaviour
 {
     public AudioSource _audioSource;
     private GameObject[] other;
     private bool NotFirst = false;
+    [SerializeField] AudioClip menuMusic;
+    [SerializeField] AudioClip gameMusic;
+    private float baseMusicVolume;
+
     private void Awake()
     {
+
         other = GameObject.FindGameObjectsWithTag("Music");
 
         foreach (GameObject oneOther in other)
@@ -25,6 +31,8 @@ public class MusicPlay : MonoBehaviour
         }
         DontDestroyOnLoad(transform.gameObject);
         _audioSource = GetComponent<AudioSource>();
+
+        baseMusicVolume = _audioSource.volume;
     }
 
     public void PlayMusic()
@@ -37,4 +45,33 @@ public class MusicPlay : MonoBehaviour
     {
         _audioSource.Stop();
     }
+
+    public void TransitionToGameMusic()
+    {
+        _audioSource.DOFade(0f, 1f).OnComplete(() => PlayMusic(gameMusic));
+    }
+
+    public void TransitionToMainMenuMusic()
+    {
+        _audioSource.DOFade(0f, 1f).OnComplete(() => PlayMusic(menuMusic));
+    }
+
+    public void PauseMusicVolume()
+    {
+        _audioSource.DOFade(0.1f, 1f);
+    }
+
+    public void ResumeMusicVolume()
+    {
+        _audioSource.DOFade(baseMusicVolume, 1f);
+    }
+
+    private void PlayMusic(AudioClip clip)
+    {
+        _audioSource.clip = clip;
+        _audioSource.DOFade(baseMusicVolume, 1f);
+        _audioSource.Play();
+    }
+
+
 }
