30170c01cca3ddff15658e1ac5345f6887f7a747
Pau Marín <paumarin96@gmail.com>
2022-07-03 14:03:18 +0200

    Ganchy art

---
+++ Assets/CharacterAnimation.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterAnimation : MonoBehaviour
+{
+    public Animator animator;
+    public CharacterMovement player;
+    public Transform GraphicsRoot;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        animator.SetFloat("Velocity", Mathf.Abs(player.playerInput.movementAxis.x));
+        animator.SetBool("Falling", player.falling);
+        animator.SetBool("Grounded", player.isGrounded);
+
+        if (Mathf.Abs(player.playerInput.movementAxis.x) > 0.1f)
+        GraphicsRoot.localScale = new Vector3(Mathf.Sign(player.playerInput.movementAxis.x),1,1);
+    }
+
+    public void Jump()
+    {
+        animator.SetTrigger("Jump");
+    }
+}
