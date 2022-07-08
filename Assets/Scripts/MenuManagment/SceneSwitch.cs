0cb009e40515e44230d3c296993cabe9f670d755
TomeuGarcia <tomeu.garcia@enti.cat>
2022-07-08 14:32:28 +0200

    Fixed audio bugs

--- Assets/Scripts/MenuManagment/SceneSwitch.cs
+++ Assets/Scripts/MenuManagment/SceneSwitch.cs
@@ -5,8 +5,16 @@ using UnityEngine.SceneManagement;
 
 public class SceneSwitch : MonoBehaviour
 {
+    public delegate void SceneSwitchAction();
+    public static event SceneSwitchAction OnGoToGameScene;
+
     public void OpenScene(int index)
     {
+        if (index == 2)
+        {
+            if (OnGoToGameScene != null) OnGoToGameScene();
+        }
+
         SceneManager.LoadScene(index);
     }
 
